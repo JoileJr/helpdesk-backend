9cb446cd5cfc0dab06c78c8faf2d597f349fa772
Joíle Júnior <joilejunior2@gmail.com>
2023-04-09 20:24:19 -0300

    first commit

---
+++ src/test/java/com/joile/helpdesk/HelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joile.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
