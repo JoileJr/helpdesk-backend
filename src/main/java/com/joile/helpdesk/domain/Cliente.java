810c731a18c69d86d473745b97cc86b30b07432c
Joíle Júnior <joilejunior2@gmail.com>
2023-04-10 12:43:27 -0300

    anotações jpa para classes de dominio

--- src/main/java/com/joile/helpdesk/domain/Cliente.java
+++ src/main/java/com/joile/helpdesk/domain/Cliente.java
@@ -3,8 +3,14 @@ package com.joile.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Cliente extends Pessoa {
+	private static final long serialVersionUID = 1L;
 	
+	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Cliente() {
