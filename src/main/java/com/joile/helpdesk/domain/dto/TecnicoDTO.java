2d8df5c213a68ee21a3f82c25f5bc7c4aafb4775
JoileJr <joilejunior2@gmail.com>
2023-10-22 10:29:54 -0300

    feat: validação de campos requeridos

--- src/main/java/com/joile/helpdesk/domain/dto/TecnicoDTO.java
+++ src/main/java/com/joile/helpdesk/domain/dto/TecnicoDTO.java
@@ -9,14 +9,19 @@ import java.util.stream.Collectors;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.joile.helpdesk.domain.Tecnico;
 import com.joile.helpdesk.domain.enums.Perfil;
+import jakarta.validation.constraints.NotNull;
 
 public class TecnicoDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	protected Integer id;
+	@NotNull(message = "O campo NOME é requerido")
 	protected String nome;
+	@NotNull(message = "O campo CPF é requerido")
 	protected String cpf;
+	@NotNull(message = "O campo EMAIL é requerido")
 	protected String email;
+	@NotNull(message = "O campo SENHA é requerido")
 	protected String senha;
 	protected Set<Integer> perfis = new HashSet<>();
 	
